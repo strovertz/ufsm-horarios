43b969ae36fe43924101a40ceac9a30e882854bc
Marcos Visentini <marcosvisentini@gmail.com>
2023-07-17 14:17:56 -0300

    Aprimora endpoint principal da API, permitindo parâmetros de filtragem

--- api/exception/CursoInvalidoError.py
+++ api/exception/CursoInvalidoError.py
@@ -4,18 +4,16 @@ from exception.UFSMHorariosError import UFSMHorariosError
 
 
 class CursoInvalidoError(UFSMHorariosError):
-    http_status_code: HTTPStatus
-    message: str
     nome_curso: str
     nome_campus: str | None
 
     def __init__(self, nome_curso: str, nome_campus: str | None):
-        self.http_status_code = HTTPStatus.NOT_FOUND
         self.nome_curso = nome_curso
 
         if nome_campus:
-            self.message = f'A UFSM não oferta o curso "{nome_curso}" em "{nome_campus}"'
+            message = f'A UFSM não oferta o curso "{nome_curso}" em "{nome_campus}"'
+            self.nome_campus = nome_campus
         else:
-            self.message = f'A UFSM não oferta o curso "{nome_curso}"'
+            message = f'A UFSM não oferta o curso "{nome_curso}"'
 
-        super().__init__(self.http_status_code, self.message)
+        super().__init__(HTTPStatus.NOT_FOUND, message)
